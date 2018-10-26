63816c77629f7ffacf5314f60547bdd5a021ce7f
Chiedo <chiedo@chiedo.com>
2018-10-26 11:09:55 -0400

    Add title persistance

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 console.log("Distraction Free Gmail Enabled.");
 
 // We don't want to see unread count in the title either
-setTimeout(function() { document.title = 'Gmail'; }, 1000);
+setInterval(function() { document.title = 'Gmail'; }, 100);
